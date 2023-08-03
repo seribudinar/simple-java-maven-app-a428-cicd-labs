cc6d109c22a79ecc8110c7847d7d65744f836de0
Edhy Khalifatullah <edhy.khal@gmail.com>
2023-08-03 15:01:25 +0700

    edit message

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello Dicoding!";
+    private static final String MESSAGE = "Hello Dicoding! Apa kabar?";
 
     public App() {
     }
