a8119b2d44c271854f6f7fee4bbea9d7b875ef72
Edhy Khalifatullah <edhy.khal@gmail.com>
2023-08-04 09:38:27 +0700

    tes message display

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -18,6 +18,6 @@ public class AppTest {
     @Test
     public void testAppMessage() {
         App app = new App();
-        assertEquals("Hello World!", app.getMessage());
+        assertEquals("Hello Dicoding! Apa kabar?", app.getMessage());
     }
 }
