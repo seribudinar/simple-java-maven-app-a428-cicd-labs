60d88a75e26fcd902bb139d51bc5b478eb235447
Edhy Khalifatullah <edhy.khal@gmail.com>
2023-08-25 10:56:20 +0700

    z

--- jenkins/scripts/build.sh
+++ jenkins/scripts/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-scp target/${NAME}-${VERSION}.jar ${REMOTE_USER}@${REMOTE_SERVER}:/home/ubuntu
-# sftp target/${NAME}-${VERSION}.jar ubuntu@18.141.186.62:/home/ubuntu
+# scp target/${NAME}-${VERSION}.jar ${REMOTE_USER}@${REMOTE_SERVER}:/home/ubuntu
+scp target/${NAME}-${VERSION}.jar ubuntu@18.141.186.62:/home/ubuntu
 
 # scp target/${NAME}-${VERSION}.jar
 # ssh -o StrictHostKeyChecking=no ${REMOTE_USER}@${REMOTE_SERVER}:/home/ubuntu
